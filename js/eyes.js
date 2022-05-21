697503274c4665911aca19fe1af53eb22f3f03f0
mxrch <kvwaljam@protonmail.ch>
2022-05-21 13:17:41 +0200

    👀

---
+++ js/eyes.js
@@ -0,0 +1,10 @@
+var _paq = window._paq = window._paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+var u="//suchanalytik.hideandsec.sh/";
+_paq.push(['setTrackerUrl', u+'analytik.php']);
+_paq.push(['setSiteId', '2']);
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.async=true; g.src=u+'analytik.js'; s.parentNode.insertBefore(g,s);
+})();
